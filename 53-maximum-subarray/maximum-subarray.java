d937407a3aa27cafcd0bc713caf92f3785555979
Arijeet Aman <100380487+arij33tt@users.noreply.github.com>
2026-08-02 13:35:02 +0530

    Time: 1 ms (99.95%) | Memory: 81 MB (5.83%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -4,20 +4,20 @@ class Solution {
         int sum=0;
         if(arr.length==1) return arr[0];
 
-        for(int i=0;i<arr.length;i++)
+       for (int i =0;i<arr.length ;i++)
+       {
+        sum+=arr[i];
+
+        if(sum>ans)
         {
-            sum+=arr[i];
-            if(sum>ans)
-            {
-                ans=sum;
-            }
-
-            if (sum<0){
-                sum=0;
-            }
-
+            ans = sum;
         }
-        return ans;
+
+        if (sum<0) sum=0;
+
+       }
+
+       return ans;
 
 
 

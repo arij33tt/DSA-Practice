fdec383ef504b3923fc86b7bf09143101ad542d0
Arijeet Aman <100380487+arij33tt@users.noreply.github.com>
2026-08-11 14:53:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (78.57%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,36 +1,36 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int [] res = new int [m+n];
-        int k=0;
-        int i=0;
-        int j=0;
+        // int j=n-1;
+        // int i=m-1;
+        // int k = m+n-1;
 
-        while (i < m && j <n)
+        // while(j>=0)
+        // {
+        //     if(i>=0 && nums1[i]>= nums2[j])
+        //     {
+        //         nums1[k--] = nums1[i--];
+        //     }
+
+        //     else {
+        //         nums1[k--]= nums2[j--];
+        //     }
+        // }
+
+        int k=m+n-1;
+        int i=m-1;
+        int j=n-1;
+
+        while(j>=0)
         {
-            if(nums1[i]<nums2[j])
+            if(i>=0 &&nums1[i]>nums2[j])
             {
-                res[k++]=nums1[i++];
+                nums1[k--]=nums1[i--];
             }
 
             else {
-                res[k++]= nums2[j++];
+                nums1[k--]=nums2[j--];
             }
         }
 
-        while (i<m)
-        {
-            res[k++]= nums1[i++];
-        }
-
-        while(j<n)
-        {
-            res[k++]= nums2[j++];
-        }
-        int h =0;
-        for(int e : res)
-        {
-            nums1[h++]= e;
-        }
-        System.out.println(Arrays.toString(res));
     }
 }
